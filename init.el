9a17ffc6419585325a5f24612e8ea1ac39e259a5
Ali Ali <aali56@students.kennesaw.edu>
2025-04-06 14:37:32 -0400

    changed .emacs to init.el, added early-init.el to set environment variable to let lsp use plists for better performance

--- init.el
+++ init.el
@@ -508,58 +508,58 @@ Version: 2024-12-20"
   )
 
 
-;(use-package minuet
-;:ensure t
-;:bind
-;(("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
-;    ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
-;    ("C-c m" . #'minuet-configure-provider)
-;    :map minuet-active-mode-map
-;    ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
-;    ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
-;    ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
-;    ("M-A" . #'minuet-accept-suggestion) ;; accept whole completion
-;    ;; Accept the first line of completion, or N lines with a numeric-prefix:
-;    ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
-;    ("M-a" . #'minuet-accept-suggestion-line)
-;    ("M-e" . #'minuet-dismiss-suggestion))
-;
-;:init
-;;; if you want to enable auto suggestion.
-;;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
-;(add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
-;:config
-;(setq minuet-provider 'openai-fim-compatible)
-;(setq minuet-n-completions 1) ; recommended for Local LLM for resource saving
-;;; I recommend beginning with a small context window size and incrementally
-;;; expanding it, depending on your local computing power. A context window
-;;; of 512, serves as an good starting point to estimate your computing
-;;; power. Once you have a reliable estimate of your local computing power,
-;;; you should adjust the context window to a larger value.
-;(setq minuet-context-window 512)
-;(plist-put minuet-openai-fim-compatible-options :end-point "http://localhost:8012/v1/completions")
-;;; an arbitrary non-null environment variable as placeholder
-;(plist-put minuet-openai-fim-compatible-options :name "Llama.cpp")
-;(plist-put minuet-openai-fim-compatible-options :api-key "TERM")
-;;; The model is set by the llama-cpp server and cannot be altered
-;;; post-launch.
-;(plist-put minuet-openai-fim-compatible-options :model "PLACEHOLDER")
-;
-;;; Llama.cpp does not support the `suffix` option in FIM completion.
-;;; Therefore, we must disable it and manually populate the special
-;;; tokens required for FIM completion.
-;(minuet-set-optional-options minuet-openai-fim-compatible-options :suffix nil :template)
-;(minuet-set-optional-options
-;minuet-openai-fim-compatible-options
-;:prompt
-;(defun minuet-llama-cpp-fim-qwen-prompt-function (ctx)
-;    (format "<|fim_prefix|>%s\n%s<|fim_suffix|>%s<|fim_middle|>"
-;            (plist-get ctx :language-and-tab)
-;            (plist-get ctx :before-cursor)
-;            (plist-get ctx :after-cursor)))
-;:template)
-;
-;(minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 56))
+(use-package minuet
+:ensure t
+:bind
+(("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
+    ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
+    ("C-c m" . #'minuet-configure-provider)
+    :map minuet-active-mode-map
+    ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
+    ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
+    ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
+    ("TAB" . #'minuet-accept-suggestion) ;; accept whole completion
+    ;; Accept the first line of completion, or N lines with a numeric-prefix:
+    ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
+    ("M-a" . #'minuet-accept-suggestion-line)
+    ("M-e" . #'minuet-dismiss-suggestion))
+
+:init
+;; if you want to enable auto suggestion.
+;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
+(add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
+:config
+(setq minuet-provider 'openai-fim-compatible)
+(setq minuet-n-completions 1) ; recommended for Local LLM for resource saving
+;; I recommend beginning with a small context window size and incrementally
+;; expanding it, depending on your local computing power. A context window
+;; of 512, serves as an good starting point to estimate your computing
+;; power. Once you have a reliable estimate of your local computing power,
+;; you should adjust the context window to a larger value.
+(setq minuet-context-window 250)
+(plist-put minuet-openai-fim-compatible-options :end-point "http://localhost:8012/v1/completions")
+;; an arbitrary non-null environment variable as placeholder
+(plist-put minuet-openai-fim-compatible-options :name "Llama.cpp")
+(plist-put minuet-openai-fim-compatible-options :api-key "TERM")
+;; The model is set by the llama-cpp server and cannot be altered
+;; post-launch.
+(plist-put minuet-openai-fim-compatible-options :model "PLACEHOLDER")
+
+;; Llama.cpp does not support the `suffix` option in FIM completion.
+;; Therefore, we must disable it and manually populate the special
+;; tokens required for FIM completion.
+(minuet-set-optional-options minuet-openai-fim-compatible-options :suffix nil :template)
+(minuet-set-optional-options
+minuet-openai-fim-compatible-options
+:prompt
+(defun minuet-llama-cpp-fim-qwen-prompt-function (ctx)
+    (format "<|fim_prefix|>%s\n%s<|fim_suffix|>%s<|fim_middle|>"
+            (plist-get ctx :language-and-tab)
+            (plist-get ctx :before-cursor)
+            (plist-get ctx :after-cursor)))
+:template)
+
+(minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 56))
 
 
 (use-package emacs
