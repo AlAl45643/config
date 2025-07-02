da2bdffe987c3abd4a15376cc48cba22e5845ac9
AlAl <alalwork@protonmail.com>
2025-07-01 20:51:48 -0400

    Give Omnisharp execute perms; download dotnet

--- .emacs.d/setup.sh
+++ .emacs.d/setup.sh
@@ -8,13 +8,15 @@ mkdir omnisharp
 cd omnisharp
 wget https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.13/omnisharp-linux-x64-net6.0.zip
 unzip omnisharp-linux-x64-net6.0.zip
+chmod +x ./OmniSharp
 cd ../../../
 mkdir debug-adapters/
 cd debug-adapters
 wget https://github.com/Samsung/netcoredbg/releases/download/3.1.2-1054/netcoredbg-linux-amd64.tar.gz
 tar -xf netcoredbg-linux-amd64.tar.gz
-cd ../../../
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
-mv JetBrainsMono-2.304.zip ~/.local/share/fonts/
-cd ~/.local/share/fonts/
-unzip JetBrainsMono-2.304.zip
+cd ../
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --version latest
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc 
+echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc 
